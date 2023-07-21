1146fbfdbe70892b0262449a373f2098429dc714
Shubham Rajput <rajputofficialshubham93029@gmail.com>
2023-07-21 12:32:38 +0530

    Login branch merged to Main branch

--- infolocker/src/App.js
+++ infolocker/src/App.js
@@ -1,12 +1,22 @@
 import BackgroundWrapper from "./components/backgroundWrapper/BackgroundWrapper";
 import Login from "./components/logincomponent/Login";
 import LoginForm from "./components/logincomponent/LoginForm";
+import {BrowserRouter , Routes  , Route } from 'react-router-dom';
 
 
 function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+
+
+        <Route path="/" element={<Login/>} />
+      </Routes>
     
-    <Login/>
+    
+    
+    </BrowserRouter>
+    
       
   );
 }
